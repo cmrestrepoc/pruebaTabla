ad13c866416b24717c6679a61768070b73da63f1
Carlos Mario Restrepo Criollo <carlos.restrepo@vasslatam.com>
2019-11-18 15:12:11 -0500

    Se resuelve problema al guardar actas cuando se piden permisos de ubicación

--- js/sw-db.js
+++ js/sw-db.js
@@ -1814,6 +1814,10 @@ function guardarEvaluacion(formulario){
 			evaluado = Object.assign(evaluado, coordinates)
 			localStorage.setItem('evaluado', JSON.stringify(evaluado));
 			firmaEvaluacion();
+		}, (err) => {
+			console.log('Error obteniendo coordenadas', err)
+			localStorage.setItem('evaluado', JSON.stringify(evaluado));
+			firmaEvaluacion();
 		})
 		
 		//persistirEvaluado(db, evaluado, formulario);
