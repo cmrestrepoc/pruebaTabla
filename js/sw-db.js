3b9828ca8f6edec59c74b832da0a7f13f1f836d9
Carlos Restrepo <carlos.restrepo@gc.vasslatam.com>
2020-10-11 07:19:02 -0500

    se remueve return que no sirve

--- js/sw-db.js
+++ js/sw-db.js
@@ -586,7 +586,6 @@ function escogerInscrito(registro, formulario){
 			let arreglo = Array.from(alerta); //en este caso alerta es un iterable pero no un arreglo, hay que convertirlo primero
 			for ( const item of arreglo) {
 				item.style.display = "none";
-				return item;
 			};
 			document.getElementsByName('entidad' + formulario).value = registro.ENTIDAD;
 		}
