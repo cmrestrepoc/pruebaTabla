ee98a8e9c913b88b90f2478b39118a76f773ba3f
CARLOS MARIO RESTREPO <cmrestrepoc@unal.edu.co>
2021-10-23 19:50:36 -0500

    Se corrige url basee

--- main.js
+++ main.js
@@ -931,7 +931,7 @@ var motivos = [
 	{objeto: tiposInscripcion, nombre: 'tiposInscripcion'},
 ]
 
-const BASE_URL_SERVICIOS = 'https://simarpro.com/visaludservercandelaria/api';
+const BASE_URL_SERVICIOS = 'https://simarpro.com/visaludservercandelaria/api/';
 
 function createOption({value, text}){
 	let option = document.createElement('option');
